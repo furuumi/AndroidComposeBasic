d676f37d1712b1699a7763abbdb0c875e63a1f9c
furuumi <>
2023-10-20 15:49:05 +0900

    テーマのカラーを修正 ただAPI レベル 31以上だとアダプティブ カラーが適用されるのでこれだけだと色は変わらなかった

--- app/src/main/java/com/android/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/android/example/basiccodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.android.example.basiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -28,6 +29,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.example.basiccodelab.ui.theme.BasicCodelabTheme
@@ -93,7 +95,9 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             ElevatedButton(
                 onClick = { expanded.value = !expanded.value }
@@ -124,23 +128,37 @@ fun OnboardingScreen(
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 480,
+)
 @Composable
-fun MyAppPreview() {
-    BasicCodelabTheme {
-        MyApp(Modifier.fillMaxSize())
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     BasicCodelabTheme {
         Greetings()
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 480,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Composable
+fun DarkPreview() {
+    BasicCodelabTheme {
+        Greetings()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320
+)
 @Composable
 fun OnboardingPreview() {
     BasicCodelabTheme {
